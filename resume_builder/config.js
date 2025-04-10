740517c20f3f809c74b6642e2ae862d98da24890
sourav-jyoti <souravjyoti64@gmail.com>
2025-04-11 01:08:13 +0530

    changes regarding deployment

--- resume_builder/config.js
+++ resume_builder/config.js
@@ -1,4 +1,6 @@
+const REACT_APP_API_URL = 'https://ai-resume-builder-jscy.onrender.com'
+const NODE_ENV = 'production'
 
-export const API_URL = process.env.NODE_ENV === 'production'
-  ? process.env.REACT_APP_API_URL
+export const API_URL = NODE_ENV === 'production'
+  ? REACT_APP_API_URL
   : 'http://localhost:3000';
