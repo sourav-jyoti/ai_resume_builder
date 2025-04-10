b597d284a6589c08e74566f2ed1f9340514f03b5
sourav-jyoti <souravjyoti64@gmail.com>
2025-04-11 01:55:05 +0530

    changes regarding deployment

--- resume_builder/config.js
+++ resume_builder/config.js
@@ -1,4 +1,4 @@
-const REACT_APP_API_URL = 'https://ai-resume-builder-jscy.onrender.com'
+const REACT_APP_API_URL = 'https://ai-resume-builder-back-bewo.onrender.com'//backened render url
 const NODE_ENV = 'local'
 
 export const API_URL = NODE_ENV === 'production'
