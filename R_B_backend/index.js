90062ee69202ec53f4099c36f3294ffe8f72f826
sourav-jyoti <souravjyoti64@gmail.com>
2025-04-11 02:09:45 +0530

    minor changes regarding deployment

--- R_B_backend/index.js
+++ R_B_backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 //midlleware allowing 5173
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? "process.env.FRONTEND_URL "
+    ? process.env.FRONTEND_URL
     : "http://localhost:5173",
   methods: "GET,POST,PUT,DELETE",
 })); 
